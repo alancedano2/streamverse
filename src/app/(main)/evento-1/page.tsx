e0d97a46b4816d2f5f7ff550818d60b1fdec87b5
alancedano2 <supernatura94@yahoo.com>
2025-07-10 15:15:07 -0400

    Update page.tsx

--- src/app/(main)/evento-1/page.tsx
+++ src/app/(main)/evento-1/page.tsx
@@ -33,7 +33,7 @@ function getF1RaceDetails(): StreamDetails {
     title: 'Katie Taylor vs. Amanda Serrano - Netflix PPV', // Título genérico de F1, ej: "Gran Premio de España"
     description: 'Este evento marca un momento crucial en el boxeo: Katie Taylor y Amanda Serrano se enfrentan en el pesaje oficial antes de su esperada pelea. Esta es una cartelera histórica compuesta solo por mujeres, presentada por Most Valuable Promotions.',
     league: 'Netflix PPV',
-    playbackUrl: 'https://tvpass.org/live/WNBCDT1/sd', // <<-- DEJA ESTA URL EN BLANCO. ¡Aquí pegarás tu stream M3U8 cuando lo tengas!
+    playbackUrl: 'https://mediaiptvproxy.fraelvillegasplay8.workers.dev/?url=https://62b7ae90565b.ngrok-free.app/LiveApp/streams/dWLk8vpJbhK6e0CU297035900555.m3u8', // <<-- DEJA ESTA URL EN BLANCO. ¡Aquí pegarás tu stream M3U8 cuando lo tengas!
     posterUrl: 'https://dnm.nflximg.net/api/v6/BvVbc2Wxr2w6QuoANoSpJKEIWjQ/AAAAQRxxf7fNC-uSsN6DlinZnFNwxAdwS3Lw4WExAlaMA-3mLLMc4t7tRuwIJ326z05IlYzECtJ9s4i0osG7BoI4oLXjjEaCC9y5IqUfQ38J56A7AGxPrOPrBPMA42g7CVmRxXdO2na6NEs48XytcyfhQ4Cm4_Q.jpg?r=5e1', // Póster genérico de F1 (dominio permitido en next.config.ts)
     isLive: isLiveNow, // Indica si está en vivo
     nextEpisodeDate: `Hoy, ${today.toLocaleDateString('es-ES', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}`, // Muestra la fecha de mañana
