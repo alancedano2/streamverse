e3b1e7b17b187a60df8f9e5b3b80dd3edc1cbf43
alancedano2 <supernatura94@yahoo.com>
2025-07-14 00:47:24 -0400

    Update page.tsx

--- src/app/(main)/evento-2/page.tsx
+++ src/app/(main)/evento-2/page.tsx
@@ -24,7 +24,7 @@ function getF1RaceDetails(): StreamDetails {
         description: 'Prepárate para la historia. WWE Evolution está de vuelta, el evento que celebra exclusivamente a las superestrellas femeninas. Después de siete años, este evento icónico regresa para redefinir la lucha libre.',
         league: 'WWE PLE',
         // Using the HTTPS proxy URL
-        playbackUrl: 'https://strengths-cutting-normal-excluded.trycloudflare.com/stream/live/991943952/e2zQtWgRZ5D4/559675.m3u8',
+        playbackUrl: 'https://pat-contests-skilled-mixer.trycloudflare.com/stream/live/N3SON3XCHANGE@NOW/GhUtuAU7rR29/39365.m3u8',
         posterUrl: 'https://tvazteca.brightspotcdn.com/75/ce/b41197bd46a3867b09f504e0ddf7/wwe-evolution-2025.jpg',
         isLive: isLiveNow,
         nextEpisodeDate: `Hoy, ${today.toLocaleDateString('es-ES', {
