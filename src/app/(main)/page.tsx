26bfdefa641f2fc71371d813c61fa6884f485f6e
alancedano2 <supernatura94@yahoo.com>
2025-07-22 12:54:13 -0400

    Update page.tsx

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -62,13 +62,29 @@ export default function HomePage() {
 
   const heroEvents = [
   {
+    id: 'MLB',
+    title: 'MLB: Toronto Blue Jays vs New York Yankees',
+    date: 'Martes, 22 de Julio del 2025 - 7:07pm',
+    description: '¡Un emocionante enfrentamiento de la División Este! Los Blue Jays reciben a los Yankees en un partido clave de la MLB. Prepárate para la acción del béisbol con cada lanzamiento y cada bateo.', // Corrected missing quote
+    bgImage: 'https://s.secure.espncdn.com/stitcher/artwork/16x9.jpg?height=612&width=1088&cb=12&templateId=espn.core.dtc.large.16x9.1&source=https%3A//artwork.espncdn.com/events/401422027/16x9/1280x720_20220328205353.jpg&showBadge=true&package=ESPN_PLUS_MLB',
+    link: 'https://streamverse-eventos.vercel.app/eventos/mlb-bluejays-yankees', // ✅ COMA AÑADIDA AQUÍ
+  },
+  {
     id: 'WWE',
-    title: 'WWE Raw',
-    date: 'Lunes, 21 de Julio del 2025 - 8:00pm',
-    description: '¡La noche de los lunes se enciende con WWE Raw! Prepárate para combates impresionantes, rivalidades intensas y sorpresas inesperadas con tus superestrellas favoritas.', // Corrected missing quote
-    bgImage: 'https://prod.cosm-cdn.io/venue-events/Sports/WWE/03_03_monday_raw/Program/All_Venues/Event_Assets/Event_Images/WWE-RAW-generic.webp',
-    link: 'https://streamverse-eventos.vercel.app/eventos/wwe-raw', // ✅ COMA AÑADIDA AQUÍ
-  }, // <--- COMA CORREGIDA AÑADIDA AQUÍ 
+    title: 'WWE NXT',
+    date: 'Lunes, 22 de Julio del 2025 - 8:00pm',
+    description: '¡La noche de los martes se enciende con WWE NXT! Prepárate para combates impresionantes, rivalidades intensas y sorpresas inesperadas con tus superestrellas favoritas.', // Corrected missing quote
+    bgImage: 'https://images.cwtv.com/images/masters/cw/universal/shows/wwe-nxt/show-promo.jpg',
+    link: 'https://streamverse-eventos.vercel.app/eventos/wwe-nxt', // ✅ COMA AÑADIDA AQUÍ
+  },
+  {
+    id: 'MLS',
+    title: 'MLS All-Star Skills Challenge',
+    date: 'Martes, 22 de Julio del 2025 - 9:00pm',
+    description: '¡Prepárate para la acción y la habilidad! Las estrellas de la MLS compiten en un emocionante desafío de habilidades previo al partido de estrellas. Duelos de precisión, control de balón y más.', // Corrected missing quote
+    bgImage: 'https://is1-ssl.mzstatic.com/image/thumb/BgtyY1s-04azDkgn3QooMQ/580x326Sports.TVAMCR01.webp',
+    link: 'https://streamverse-eventos.vercel.app/eventos/mls-all-star-skills-challenge', // ✅ COMA AÑADIDA AQUÍ
+  },// <--- COMA CORREGIDA AÑADIDA AQUÍ 
 ];
 
   const trendingEvents = [
